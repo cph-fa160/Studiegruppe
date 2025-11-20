c441159c003a4a68045e4cbf2179be82d6927cdc
MadsBendz02 <cph-mb955@stud.ek.dk>
2025-11-20 15:12:42 +0100

    OLA 4 DDL

---
+++ OLA4_DDL.sql
@@ -0,0 +1,32 @@
+CREATE DATABASE Bilbasen;
+USE Bilbasen;
+
+-- Invariant: dealer
+CREATE TABLE dealer (
+    dealer_id      VARCHAR(255) PRIMARY KEY,
+    seller_name    VARCHAR(255),
+    seller_address VARCHAR(255),
+    seller_cvr     VARCHAR(20)
+);
+
+-- Invariant: car
+CREATE TABLE car (
+    carid       VARCHAR(255) PRIMARY KEY,
+    makemodel   VARCHAR(255),
+    props       VARCHAR(255),
+    link        VARCHAR(500),
+    dealer_id   VARCHAR(10),
+    FOREIGN KEY (dealer_id) REFERENCES dealer(dealer_id)
+);
+
+
+-- Tidsserie: observationer på bil
+-- ==============================
+CREATE TABLE car_observation (
+    obs_id     INT AUTO_INCREMENT PRIMARY KEY,
+    carid      VARCHAR(255),
+    price      DECIMAL(10,2),
+    scrapedate DATE,
+    sold BOOLEAN NOT NULL DEFAULT FALSE,
+    FOREIGN KEY (carid) REFERENCES car(carid)
+);
